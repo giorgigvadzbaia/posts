f9d0130b5286b69e4198d10f9a4b139009aa2974
giorgigvadzbaia <167540912+giorgigvadzbaia@users.noreply.github.com>
2024-05-26 19:06:05 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,217 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+from forms import LoginForm, RegistrationForm, UserUpdateForm
+from werkzeug.utils import secure_filename
+
+from db import db, db_init
+from models import img
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, validates
+from sqlalchemy import Integer, String, SmallInteger, BigInteger
+
+app = Flask(__name__)
+
+
+class Base(DeclarativeBase):
+  pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+def db_init(app):
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str]
+    age: Mapped[int] = mapped_column(SmallInteger)
+    address: Mapped[str]
+
+
+# with app.app_context():
+#     print('Creating Database And Tables')
+#     db.create_all()
+#     print('Created Database and tablesss')
+
+
+def middle(value):
+    if len(value) % 2 == 1:
+        len_value = (len(value) - 1) / 2
+        value = value[int(len_value)]
+    return value
+
+
+def create_conn():
+    conn = sqlite3.connect('mziuri.db', check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_cursor(conn):
+    return conn.cursor()
+
+
+def close_conn(conn):
+    return conn.close()
+
+
+app.jinja_env.filters['middle'] = middle
+
+
+class image(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    image: Mapped[str] = mapped_column(unique=False, nullable=False)
+    name: Mapped[str] = mapped_column(unique=False, nullable=False)
+    mimetype: Mapped[str] = mapped_column(nullable=False)
+
+@app.route('/upload', methods=['POST'])
+def uploud():
+    picture = request.files["pic"]
+
+    if not picture:
+        flash('no pics were uploaded')
+        return  redirect(url_for("uploud")) 
+
+    file_name = secure_filename(picture.file_name)
+    mimetype = picture.mimetype
+
+    img = img(image=picture.read(), mimetype=mimetype, name=file_name)
+    db.session.add(img)
+    db.session.commit()
+
+    flash("image has been uploaded")
+    return redirect(url_for("home"))
+
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    first_name, last_name = 'nugzari', 'svianadze'
+    num = 25
+    d = {'name': 'nugzari', 'age': 20}
+    return render_template('home.html', first_name=first_name,
+                           last_name=last_name, num=num, d=d)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            print('forma validuria')
+            return redirect(url_for('home'))
+        print(form.errors)
+        return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    form = RegistrationForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            age = form.age.data
+            address = form.address.data
+
+            # conn = create_conn()
+            # cursor = create_cursor(conn)
+            # cursor.execute("""
+            # select * from users where email = ?
+            # """, (email,))
+            # user_exists = cursor.fetchone()
+            # close_conn(conn)
+            user_exists = User.query.filter_by(first_name=first_name).all()
+            if user_exists is not None:
+                # flash('User With This Email Already Exists!')
+                form.first_name.errors = ['User With This First Name Already Exists!']
+                return render_template('register.html', form=form)
+
+            # conn = create_conn()
+            # cursor = create_cursor(conn)
+            # cursor.execute("""
+            # insert into users (first_name, last_name, email, age, birth_date, password) values
+            # (?, ?, ?, ?, ?, ?)
+            # """, (first_name, last_name, email, age, birthday, password))
+            # conn.commit()
+            # close_conn(conn)
+            user = User(first_name=first_name, last_name=last_name,
+                        age=age, address=address)
+            db.session.add(user)
+            # db.session.commit()
+            flash('User Successfully Created!!')
+            return redirect(url_for('home'))
+        print(form.errors)
+        return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
+
+app.secret_key = 'ijbiazbadub84v8rbsibiewfvidvsa'
+
+
+@app.route('/users')
+def users():
+    form = UserUpdateForm()
+    # conn = create_conn()
+    # cursor = create_cursor(conn)
+    # cursor.execute("select * from users")
+    # users = cursor.fetchall()
+    # close_conn(conn)
+    users_data = User.query.all()
+    return render_template('users.html', users=users_data, form=form)
+
+
+@app.route('/update_user/<int:user_id>', methods=['GET', 'POST'])
+def update_user(user_id):
+    # form = UserUpdateForm()
+    # conn = create_conn()
+    # cursor = create_cursor(conn)
+    # first_name = form.first_name.data
+    # last_name = form.last_name.data
+    # age = form.age.data
+    # user = cursor.execute("""
+    # select * from users where id = ?
+    # """, (user_id, ))
+    # if not user.fetchone():
+    #     flash('User With This Id Does Not Exist!')
+    #     return redirect(url_for('users'))
+    # cursor.execute("""
+    # update users set first_name = ?, last_name = ?, age = ? where id = ?
+    # """, (first_name, last_name, age, user_id))
+    # flash("User Updated Successfully!!!!")
+    # conn.commit()
+    # close_conn(conn)
+    return redirect(url_for('users'))
+
+
+@app.route('/delete_user/<int:user_id>')
+def delete_user(user_id):
+    conn = create_conn()
+    cursor = create_cursor(conn)
+    user = cursor.execute("""
+        select * from users where id = ?
+        """, (user_id,))
+    if not user.fetchone():
+        flash('User With This Id Does Not Exist!')
+        return redirect(url_for('users'))
+    cursor.execute("""
+    delete from users where id = ?
+    """, (user_id,))
+    flash("User Successfully Deleted!!!!!")
+    conn.commit()
+    close_conn(conn)
+    return redirect(url_for('users'))
+if __name__ == '__main__':
+    app.run(debug=True, port=5100)
---
+++ forms.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import EmailField, StringField, PasswordField, SubmitField, IntegerField, DateField
+from wtforms.validators import data_required, email, length, ValidationError
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[email(), data_required()],
+                       render_kw={'class': 'form-control', 'placeholder': 'Enter Email'})
+    username = StringField('Username', validators=[data_required(), length(5, 30)],
+                           render_kw={'class': 'form-control', 'placeholder': 'Enter Username'})
+    password = PasswordField('Password', validators=[data_required()],
+                             render_kw={'class': 'form-control', 'placeholder': 'Enter Password'})
+    submit = SubmitField('Login')
+
+    def validate_username(self, field):
+        print(field, field.data)
+        if not field.data.isalpha():
+            raise ValidationError('Username Must not include numeric numbers')
+
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[data_required()],
+                             render_kw={'class': 'form-control', 'placeholder': 'Enter First Name'})
+    last_name = StringField('Last Name', validators=[data_required()],
+                            render_kw={'class': 'form-control', 'placeholder': 'Enter Last Name'})
+    age = IntegerField('Age', validators=[data_required()],
+                       render_kw={'class': 'form-control', 'placeholder': 'Enter Age'})
+    # birthday = DateField('Birthday', validators=[data_required()],
+    #                      render_kw={'class': 'form-control', 'placeholder': 'Enter Birthday Date'})
+    # email = EmailField('Email', validators=[data_required(), email()],
+    #                    render_kw={'class': 'form-control', 'placeholder': 'Enter Email'})
+    # password = PasswordField('Password', validators=[data_required()],
+    #                          render_kw={'class': 'form-control', 'placeholder': 'Enter Password'})
+    address = StringField('Address',
+                          render_kw={'class': 'form-control', 'placeholder': 'Enter Address'})
+    submit = SubmitField('Sign In', render_kw={'class': 'btn btn-primary'})
+
+
+class image(FlaskForm):
+    image = StringField('image', validators=[data_required()],)
+
+    name = StringField('First Name', validators=[data_required()],
+                             render_kw={'class': 'form-control', 'placeholder': 'Enter First Name'})
+    mimetype = last_name = StringField('Last Name', validators=[data_required()],
+                            render_kw={'class': 'form-control', 'placeholder': 'Enter Last Name'})
+    
+    
+    
+class UserUpdateForm(RegistrationForm):
+    birthday = None
+    email = None
+    password = None
